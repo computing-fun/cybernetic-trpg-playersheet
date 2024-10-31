e3d73bcc807240e1d7e254a5498be8e387769153
Zachary Cohen <zachary@computingfun.org>
2024-10-31 17:15:24 -0400

    start to work on ui

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
-use glib::ExitCode;
-use gui::creator;
+use std::{cell::RefCell, rc::Rc};
+
+use gtk4::{glib::ExitCode, prelude::*, Application, ApplicationWindow, Box, Button, Orientation};
+use rfd::FileDialog;
 use sheet::Catalog;
 
-pub mod archive;
-mod gui;
+mod archive;
 mod sheet;
 
 pub const APP_ID: &str = "org.computingfun.cybernetic-trpg";
 
 fn main() -> ExitCode {
-    let r = creator();
+    let r = main_menu();
     println!("{:?}", r);
     ExitCode::SUCCESS
 }
@@ -18,4 +19,125 @@ fn main() -> ExitCode {
 /// Returns [`None`] if the argument or matching catalog entry is not found.
 pub fn arg() -> Option<Catalog> {
     std::env::args_os().nth(1).and_then(Catalog::lookup)
+}
+
+#[derive(Debug)]
+pub enum MainMenuOptions {
+    Open,
+    Character,
+    Race,
+    Class,
+}
+
+pub fn main_menu() -> Option<MainMenuOptions> {
+    let result: Rc<RefCell<Option<MainMenuOptions>>> = Rc::new(RefCell::new(None));
+    let result_ref = Rc::clone(&result);
+    let app = Application::builder().application_id(APP_ID).build();
+    app.connect_activate(move |app| {
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .title("Creator")
+            .default_width(250)
+            .default_height(250)
+            .resizable(false)
+            .build();
+
+        let area = Box::new(Orientation::Vertical, 10);
+        area.set_margin_start(10);
+        area.set_margin_end(10);
+        area.set_margin_top(10);
+        area.set_margin_bottom(10);
+
+        let open_btn = Button::with_label("Open sheet");
+        let char_btn = Button::with_label("Create new character");
+        let race_btn = Button::with_label("Create new race");
+        let class_btn = Button::with_label("Create new class");
+
+        let value = result_ref.clone();
+        let window_ref = window.downgrade();
+        open_btn.connect_clicked(move |_| {
+            *value.borrow_mut() = Some(MainMenuOptions::Open);
+            if let Some(w) = window_ref.upgrade() {
+                w.close();
+            }
+        });
+
+        let value = result_ref.clone();
+        let window_ref = window.downgrade();
+        char_btn.connect_clicked(move |_| {
+            *value.borrow_mut() = Some(MainMenuOptions::Character);
+            if let Some(w) = window_ref.upgrade() {
+                w.close();
+            }
+        });
+
+        let value = result_ref.clone();
+        let window_ref = window.downgrade();
+        race_btn.connect_clicked(move |_| {
+            *value.borrow_mut() = Some(MainMenuOptions::Race);
+            if let Some(w) = window_ref.upgrade() {
+                w.close();
+            }
+        });
+
+        let value = result_ref.clone();
+        let window_ref = window.downgrade();
+        class_btn.connect_clicked(move |_| {
+            *value.borrow_mut() = Some(MainMenuOptions::Class);
+            if let Some(w) = window_ref.upgrade() {
+                w.close();
+            }
+        });
+
+        area.append(&open_btn);
+        area.append(&char_btn);
+        area.append(&race_btn);
+        area.append(&class_btn);
+
+        window.set_child(Some(&area));
+        window.show();
+    });
+    match app.run() == ExitCode::SUCCESS {
+        true => result.take(),
+        false => None,
+    }
+}
+
+/// Opens a file dialog to allow the user to pick a file, then attempts to load
+/// the corresponding `Catalog` entry based on file extension.
+/// Returns [`None`] if no file is chosen or if no matching catalog entry is found.
+pub fn file_picker() -> Option<Catalog> {
+    FileDialog::new().pick_file().and_then(Catalog::lookup)
+}
+
+/// Prompts the user to select a location to save the provided `Archivable` instance.
+/// Returns an error if the user cancels the save dialog or if the file extension is invalid.
+pub fn file_saver<A>(archivable: &A) -> Result<(), archive::Error>
+where
+    A: archive::Archivable,
+{
+    let path = match FileDialog::new()
+        .add_filter(A::EXTENSION, &[A::EXTENSION])
+        .save_file()
+    {
+        Some(path) => path,
+        None => {
+            return Err(archive::Error::IO(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "Save canceled",
+            )))
+        }
+    };
+
+    let arch = match archive::Archive::new(path) {
+        Some(arch) => arch,
+        None => {
+            return Err(archive::Error::IO(std::io::Error::new(
+                std::io::ErrorKind::InvalidInput,
+                "Invalid file extension",
+            )))
+        }
+    };
+
+    arch.record(archivable)
 }
--- src/sheet.rs
+++ src/sheet.rs
@@ -31,25 +31,74 @@ impl Catalog {
     }
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
+pub struct Ability {
+    score: isize,
+}
+
+impl Ability {
+    pub const MIN_SCORE: isize = 1;
+    pub const MAX_SCORE: isize = 30;
+
+    /// Returns the ability score.
+    pub fn score(&self) -> isize {
+        self.score
+    }
+
+    /// Calculates the modifier for the score.
+    /// Modifier is score minus 10 divided by 2, rounded down.
+    pub fn modifier(&self) -> isize {
+        self.score.saturating_sub(10).div_euclid(2)
+    }
+
+    /// Creates an `Ability` from a given score.
+    /// The score will be clamped between `MIN_SCORE` and `MAX_SCORE`.
+    pub fn from_score(score: isize) -> Self {
+        Self {
+            score: score.clamp(Ability::MIN_SCORE, Ability::MAX_SCORE),
+        }
+    }
+
+    /// Calculates the lowest score that would produce the specified modifier.
+    pub fn from_modifier_down(modifier: isize) -> Self {
+        Self::from_score(modifier.saturating_mul(2).saturating_add(10))
+    }
+
+    /// Calculates the highest score that would produce the specified modifier.
+    pub fn from_modifier_up(modifier: isize) -> Self {
+        Self::from_score(modifier.saturating_mul(2).saturating_add(11))
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct AbilityBlock {
+    /// Strength measures bodily power, athletic training, and the extent to which you can exert raw physical force.
+    pub strenght: Ability,
+
+    /// Dexterity measures agility, reflexes, and balance.
+    pub dexterity: Ability,
+
+    /// Constitution measures health, stamina, and vital force.
+    pub constitution: Ability,
+
+    /// Intelligence measures mental acuity, accuracy of recall, and the ability to reason.
+    pub intellignce: Ability,
+
+    /// Wisdom reflects how attuned you are to the world around you and represents perceptiveness and intuition.
+    pub wisdom: Ability,
+
+    /// Charisma measures your ability to interact effectively with others. It includes such factors as confidence and eloquence, and it can represent a charming or commanding personality.
+    pub charisma: Ability,
+}
+
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Character {
     pub name: String,
+    pub description: String,
+    pub speed: usize,
     pub race: Race,
-    pub class: Class,
-}
-
-impl Archivable for Character {
-    const EXTENSION: &'static str = "character-sheet";
-}
-
-impl Default for Character {
-    fn default() -> Self {
-        Self {
-            name: "Unknown".to_string(),
-            race: Default::default(),
-            class: Default::default(),
-        }
-    }
+    pub ability: AbilityBlock,
+    pub class: Vec<Class>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -57,31 +106,205 @@ pub struct Race {
     pub name: String,
 }
 
-impl Archivable for Race {
-    const EXTENSION: &'static str = "race-sheet";
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Class {
+    pub name: String,
+    pub level: usize,
 }
 
-impl Default for Race {
-    fn default() -> Self {
-        Self {
-            name: "Unknown".to_string(),
+mod meta {
+    use std::ops::{Add, Sub};
+
+    use crate::archive::Archivable;
+
+    use super::*;
+
+    // - Ability - AbilityBlock -
+
+    impl Default for Ability {
+        fn default() -> Self {
+            Self::from_score(10)
+        }
+    }
+
+    impl Add for Ability {
+        type Output = Ability;
+        fn add(self, rhs: Self) -> Self::Output {
+            Ability::from_score(self.score.saturating_add(rhs.score))
+        }
+    }
+
+    impl Sub for Ability {
+        type Output = Ability;
+        fn sub(self, rhs: Self) -> Self::Output {
+            Ability::from_score(self.score.saturating_sub(rhs.score))
+        }
+    }
+
+    impl Default for AbilityBlock {
+        fn default() -> Self {
+            Self {
+                strenght: Default::default(),
+                dexterity: Default::default(),
+                constitution: Default::default(),
+                intellignce: Default::default(),
+                wisdom: Default::default(),
+                charisma: Default::default(),
+            }
+        }
+    }
+
+    // - Character -
+
+    impl Archivable for Character {
+        const EXTENSION: &'static str = "character-sheet";
+    }
+
+    impl Default for Character {
+        fn default() -> Self {
+            Self {
+                name: "Unknown".to_string(),
+                race: Default::default(),
+                class: Default::default(),
+                description: Default::default(),
+                speed: 30,
+                ability: Default::default(),
+            }
+        }
+    }
+
+    // - Race -
+
+    impl Archivable for Race {
+        const EXTENSION: &'static str = "race-sheet";
+    }
+
+    impl Default for Race {
+        fn default() -> Self {
+            Self {
+                name: "Unknown".to_string(),
+            }
+        }
+    }
+
+    // - Class -
+
+    impl Archivable for Class {
+        const EXTENSION: &'static str = "class-sheet";
+    }
+
+    impl Default for Class {
+        fn default() -> Self {
+            Self {
+                name: "Unknown".to_string(),
+                level: 0,
+            }
         }
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct Class {
-    pub name: String,
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-impl Archivable for Class {
-    const EXTENSION: &'static str = "class-sheet";
-}
+    #[test]
+    fn ability_score_to_modifier() {
+        for (score, expected_modifier) in [
+            (1, -5),
+            (2, -4),
+            (3, -4),
+            (4, -3),
+            (5, -3),
+            (6, -2),
+            (7, -2),
+            (8, -1),
+            (9, -1),
+            (10, 0),
+            (11, 0),
+            (12, 1),
+            (13, 1),
+            (14, 2),
+            (15, 2),
+            (16, 3),
+            (17, 3),
+            (18, 4),
+            (19, 4),
+            (20, 5),
+            (21, 5),
+            (22, 6),
+            (23, 6),
+            (24, 7),
+            (25, 7),
+            (26, 8),
+            (27, 8),
+            (28, 9),
+            (29, 9),
+            (30, 10),
+        ] {
+            assert_eq!(
+                Ability::from_score(score).modifier(),
+                expected_modifier,
+                "Failed for score: {}",
+                score
+            );
+        }
+    }
 
-impl Default for Class {
-    fn default() -> Self {
-        Self {
-            name: "Unknown".to_string(),
+    #[test]
+    fn ability_score_to_modifier_down() {
+        for (modifier, expected_score) in [
+            (-5, 1),
+            (-4, 2),
+            (-3, 4),
+            (-2, 6),
+            (-1, 8),
+            (0, 10),
+            (1, 12),
+            (2, 14),
+            (3, 16),
+            (4, 18),
+            (5, 20),
+            (6, 22),
+            (7, 24),
+            (8, 26),
+            (9, 28),
+            (10, 30),
+        ] {
+            assert_eq!(
+                Ability::from_modifier_down(modifier).score(),
+                expected_score,
+                "Failed for modifier: {}",
+                modifier
+            );
+        }
+    }
+
+    #[test]
+    fn ability_score_to_modifier_up() {
+        for (modifier, expected_score) in [
+            (-5, 1),
+            (-4, 3),
+            (-3, 5),
+            (-2, 7),
+            (-1, 9),
+            (0, 11),
+            (1, 13),
+            (2, 15),
+            (3, 17),
+            (4, 19),
+            (5, 21),
+            (6, 23),
+            (7, 25),
+            (8, 27),
+            (9, 29),
+            (10, 30),
+        ] {
+            assert_eq!(
+                Ability::from_modifier_up(modifier).score(),
+                expected_score,
+                "Failed for modifier: {}",
+                modifier
+            );
         }
     }
 }
